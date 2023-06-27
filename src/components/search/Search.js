0a5f06adc0331a1c2ab08b5b6f857683ed883bc9
Adam Lang <adamjoshualang@gmail.com>
2023-06-27 21:55:28 +0300

    feat: looping over fetch request and returning city data from api

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { AsyncPaginate } from 'react-select-async-paginate';
+import { GEO_API_URL, geoApiOptions } from '../../Api';
 
 const Search = ({ onSearchChange }) => {
     const [search, setSearch] = useState(null);
@@ -9,12 +10,31 @@ const Search = ({ onSearchChange }) => {
         onSearchChange(searchData);
     };
 
+    const loadOptions = (inputValue) => {
+        return fetch(
+            `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+            geoApiOptions
+        )
+            .then((response) => response.json())
+            .then((response) => {
+                return {
+                    options: response.data.map((city) => {
+                        return {
+                            value: `${city.latitude} ${city.longitude}`,
+                            label: `${city.name} ${city.countryCode}`,
+                        };
+                    }),
+                };
+            });
+    };
+
     return (
         <AsyncPaginate
             placeholder='Search for city'
             debounceTimeout={600}
             value={search}
             onChange={handleOnChange}
+            loadOptions={loadOptions}
         />
     );
 };
